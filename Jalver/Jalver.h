5c54d4aead00e9f61e1a1bba39be6013e55907c0
Julian <julian.alonso.dev@gmail.com>
2016-02-22 21:13:16 +0100

    Initial Commit

---
+++ Jalver/Jalver.h
@@ -0,0 +1,19 @@
+//
+//  Jalver.h
+//  Jalver
+//
+//  Created by Julian Alonso on 22/2/16.
+//  Copyright © 2016 Jalver. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Jalver.
+FOUNDATION_EXPORT double JalverVersionNumber;
+
+//! Project version string for Jalver.
+FOUNDATION_EXPORT const unsigned char JalverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jalver/PublicHeader.h>
+
+
