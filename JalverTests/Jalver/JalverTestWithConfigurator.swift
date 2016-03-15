89bb60230288924caf359fe62cb3a15c30909d44
Julian <julian.alonso.dev@gmail.com>
2016-03-16 00:13:42 +0100

    Tested that configurator can be a struct, also tested that Configurator works with final classes

--- JalverTests/Jalver/JalverTestWithConfigurator.swift
+++ JalverTests/Jalver/JalverTestWithConfigurator.swift
@@ -17,11 +17,18 @@ class JalverTestWithConfigurator: XCTestCase {
         XCTAssertEqual(solvedVehicle.model, "A3", "Vehicle model must be equal to A3")
     }
     
+    func testCreatingCarFinalClassWithConfiguratorModelA3BrandAudi() {
+        let solvedCar = Jalver.resolve(CarConfigurator.self) { (inout configurator: CarConfigurator) -> Void in
+            configurator.brand = "Audi"
+        }
+        XCTAssertEqual(solvedCar.model, "A3", "Car model must be equal to A3")
+        XCTAssertEqual(solvedCar.brand, "Audi", "Car brand must be equal to Audi")
+    }
+    
     func testCreatingPersonStructWithConfiguratorAndNamePaco() {
         let person = Jalver.resolve(PersonConfigurator.self) { (inout configurator: PersonConfigurator) -> Void in
             configurator.name = "Paco"
         }
-        
         XCTAssertEqual(person.name, "Paco", "Person name must be equal to Paco")
     }
     
@@ -34,6 +41,17 @@ private final class VehicleConfigurator: Configurator {
     
     func configure() -> Vehicle {
         return Vehicle(model: self.model)
+    }
+    
+}
+
+//With this also test that configurator can be a struct.
+private struct CarConfigurator: Configurator {
+    
+    var brand: String!
+    
+    func configure() -> Car {
+        return Car(model: "A3", brand: self.brand)
     }
     
 }
