d01054fe476d7f77e907a4bbd3389edd3c02f7b4
Julian <julian.alonso.dev@gmail.com>
2016-03-16 15:10:37 +0100

    Added .Default and .Singleton scopes

---
+++ Sources/Jalver/CommonProtocols/Scopes/JalverScopeable.swift
@@ -0,0 +1,34 @@
+//
+//  JalverScopeable.swift
+//  Jalver
+//
+//  Created by Julian Alonso on 16/3/16.
+//  Copyright © 2016 Jalver. All rights reserved.
+//
+
+import Foundation
+
+public enum JalverScope {
+    case Singleton
+    case Default
+}
+
+public protocol JalverScopeable {
+    
+    func getScope() -> JalverScope
+    
+}
+
+extension JalverScopeable {
+    
+    var scope: JalverScope {
+        get {
+            return self.getScope()
+        }
+    }
+    
+    func getScope() -> JalverScope {
+        return .Default
+    }
+    
+}
